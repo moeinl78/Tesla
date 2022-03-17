e782acca9692bad7d87689ff389189087fbcb777
Moein Latifi <moeinl1090@protonmail.com>
2022-03-18 00:07:52 +0330

    completed design of second main activity

--- app/src/main/java/ir/ariyana/tesla/MainActivitySecond.kt
+++ app/src/main/java/ir/ariyana/tesla/MainActivitySecond.kt
@@ -2,10 +2,29 @@ package ir.ariyana.tesla
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.view.isVisible
+import ir.ariyana.tesla.databinding.ActivityMainSecondBinding
 
 class MainActivitySecond : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainSecondBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_second)
+        binding = ActivityMainSecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val lighters = arrayListOf(
+            binding.secondMainActivityLighterFirst,
+            binding.secondMainActivityLighterSecond,
+            binding.secondMainActivityLighterThird,
+            binding.secondMainActivityLighterForth,
+        )
+
+        lighters.map { item ->
+            item.setOnClickListener {
+                item.visibility = View.VISIBLE
+            }
+        }
     }
 }
