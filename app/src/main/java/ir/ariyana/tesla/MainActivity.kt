df9b93d6cb17ba8a360dd5c9491657dae53c0c43
Moein Latifi <moeinl1090@protonmail.com>
2022-03-17 16:57:21 +0330

    added neumorphism style to the project

---
+++ app/src/main/java/ir/ariyana/tesla/MainActivity.kt
@@ -0,0 +1,15 @@
+package ir.ariyana.tesla
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ir.ariyana.tesla.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
