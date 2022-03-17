2188a4306bf7526cbee27daef624098e8fee42df
Moein Latifi <moeinl1090@protonmail.com>
2022-03-17 22:50:59 +0330

    added second activity and design for its header

--- app/src/main/java/ir/ariyana/tesla/MainActivity.kt
+++ app/src/main/java/ir/ariyana/tesla/MainActivity.kt
@@ -1,5 +1,6 @@
 package ir.ariyana.tesla
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ir.ariyana.tesla.databinding.ActivityMainBinding
@@ -11,5 +12,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.mainActivityOpenCar.setOnClickListener {
+            val intent = Intent(this, MainActivitySecond::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/ir/ariyana/tesla/MainActivitySecond.kt
@@ -0,0 +1,11 @@
+package ir.ariyana.tesla
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivitySecond : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_second)
+    }
+}
