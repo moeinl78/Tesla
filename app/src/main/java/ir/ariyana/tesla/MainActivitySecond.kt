a99fe44f49cbee61876272f0ef69dcbdabdd5c19
Moein Latifi <moeinl1090@protonmail.com>
2022-03-18 00:28:30 +0330

    added lighters visibilty

--- app/src/main/java/ir/ariyana/tesla/MainActivitySecond.kt
+++ app/src/main/java/ir/ariyana/tesla/MainActivitySecond.kt
@@ -14,17 +14,29 @@ class MainActivitySecond : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainSecondBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val lighters = arrayListOf(
-            binding.secondMainActivityLighterFirst,
-            binding.secondMainActivityLighterSecond,
-            binding.secondMainActivityLighterThird,
-            binding.secondMainActivityLighterForth,
-        )
 
-        lighters.map { item ->
-            item.setOnClickListener {
-                item.visibility = View.VISIBLE
-            }
+        binding.secondCard1.setOnClickListener {
+            var state = false
+            binding.secondMainActivityLighterFirst.isVisible = !state
+            state = !state
+        }
+
+        binding.secondCard2.setOnClickListener {
+            var state = false
+            binding.secondMainActivityLighterSecond.isVisible = !state
+            state = !state
+        }
+
+        binding.secondCard3.setOnClickListener {
+            var state = false
+            binding.secondMainActivityLighterThird.isVisible = !state
+            state = !state
+        }
+
+        binding.secondCard4.setOnClickListener {
+            var state = false
+            binding.secondMainActivityLighterForth.isVisible = !state
+            state = !state
         }
     }
 }
